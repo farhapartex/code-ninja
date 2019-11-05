2f53a31922224a135f9aee3792295642f8414856
Md Nazmul Hasan <farhapartex@gmail.com>
2019-11-06 00:16:20 +0600

    codeforce 1256-A added

--- codeforces/1256-A.py
+++ codeforces/1256-A.py
@@ -3,4 +3,4 @@ q = int(input())
 for i in range(q):
     a,b,n,s = map(int, input().split())
     t = (a*n) + (b*1)
-    print("YES" if min(s//n,a)*n+b >= s else "NO") 
+    print("YES" if min(s//n,a)*n+b >= s else "NO")  
