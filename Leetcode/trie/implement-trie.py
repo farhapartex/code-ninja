3c112b75fa75cf6f563e139e962954b5ac09bee4
Md Nazmul Hasan <farhapartex@gmail.com>
2019-11-10 14:32:24 +0600

    leetcode trie implement-trie

--- Leetcode/trie/implement-trie.py
+++ Leetcode/trie/implement-trie.py
@@ -2,9 +2,9 @@ class TrieNode:
     def __init__(self):
         self.children = [None]*26
         self.is_end_word = False
-
-
+        
 class Trie:
+
     def __init__(self):
         self.root = self.get_node()
     
@@ -13,8 +13,9 @@ class Trie:
     
     def _char_to_index(self,ch):
         return ord(ch)-ord('a')
-    
-    def insert(self,key):
+        
+
+    def insert(self, key: str) -> None:
         p_crawl = self.root
         for level in range(len(key)):
             index = self._char_to_index(key[level])
@@ -23,9 +24,9 @@ class Trie:
             p_crawl = p_crawl.children[index]
         
         p_crawl.is_end_word = True
-    
+        
 
-    def search(self,key):
+    def search(self, key: str) -> bool:
         p_crawl = self.root
         for level in range(len(key)):
             index = self._char_to_index(key[level])
@@ -33,31 +34,23 @@ class Trie:
                 return False
             p_crawl = p_crawl.children[index]
         
-        return p_crawl is not None and p_crawl.is_end_word
-    
-    def starts_with(self,prefix):
+        return p_crawl is not None and p_crawl.is_end_word 
+        
+
+    def startsWith(self, prefix: str) -> bool:
         p_crawl = self.root
         for level in range(len(prefix)):
-            index = self._char_to_index(key[level])
+            index = self._char_to_index(prefix[level])
             if not p_crawl.children[index]:
                 return False
             p_crawl = p_crawl.children[index]
         
         return True if p_crawl is not None else False
+        
 
 
-if __name__ == "__main__":
-    keys = ["the","a","there","anaswe","any", 
-            "by","their"] 
-    output = ["Not present in trie", 
-              "Present in trie"]
-    
-    t = Trie()
-
-    for key in keys:
-        t.insert(key)
-    
-    print(t.search("the"))
-    print("{} ---- {}".format("the",output[t.search("the")]))
-    print("{} ---- {}".format("these",output[t.search("these")]))
-    print("{} ---- {}".format("their",output[t.search("their")]))
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
