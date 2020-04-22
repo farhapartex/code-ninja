62d2486342645a767942b3be13859a36a7eb9476
Md Nazmul Hasan <farhapartex@gmail.com>
2020-04-23 01:01:37 +0600

    latest

--- URI/1198.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    try:
-        while True:
-            data = [int(n) for n in input().split()]
-            res = data[0]-data[1]
-            print(res*(-1)) if res<0 else print(res)
-    except:
-        pass
